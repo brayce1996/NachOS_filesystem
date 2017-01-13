649d792b8b85fc5de06c704c35bfdf1270ec2491
brayce <brayce1996@gmail.com>
2017-01-14 01:34:10 +0800

    transfer directory name to fit TA's testcase

--- code/threads/main.cc
+++ code/threads/main.cc
@@ -156,7 +156,19 @@ static void
 CreateDirectory(char *name)
 {
 	kernel->fileSystem->Create(name,256);
+
+    //if(name!=NULL) delete name;
 }
+
+static char* TransferName(char * name){
+    if(name[strlen(name)-1]=='/') return name;
+
+    char * transferName = new char[strlen(name)+1];
+    sprintf(transferName,"%s/",name);
+
+    return transferName;
+}
+
 
 //----------------------------------------------------------------------
 // main
@@ -250,6 +262,8 @@ main(int argc, char **argv)
 		// MP4 mod tag
 		ASSERT(i + 1 < argc);
 		listDirectoryName = argv[i + 1];
+        listDirectoryName = TransferName(listDirectoryName);
+
 		dirListFlag = true;
 		i++;
 	}
@@ -258,6 +272,8 @@ main(int argc, char **argv)
 		// recursive list
 		ASSERT(i + 1 < argc);
 		listDirectoryName = argv[i + 1];
+        listDirectoryName = TransferName(listDirectoryName);
+
 		dirListFlag = true;
 		recursiveListFlag = true;
 		i++;
@@ -266,6 +282,7 @@ main(int argc, char **argv)
 		// MP4 mod tag
 		ASSERT(i + 1 < argc);
 		createDirectoryName = argv[i + 1];
+        createDirectoryName = TransferName(createDirectoryName);
 		mkdirFlag = true;
 		i++;
 	}
@@ -322,6 +339,7 @@ main(int argc, char **argv)
     }
 	if (mkdirFlag) {
 		// MP4 mod tag
+
 		CreateDirectory(createDirectoryName);
 	}
     if (printFileName != NULL) {
